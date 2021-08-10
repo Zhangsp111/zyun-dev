ff6cc1d0a8ce9690fdd9d1a718ad906f3bbcafd4
张帅帅 <347252049@qq.com>
2021-08-10 20:07:00 +0800

    master第一次测试push

--- z-yun-system-manager/src/main/java/com/zyun/system/SystemManagerApplication.java
+++ z-yun-system-manager/src/main/java/com/zyun/system/SystemManagerApplication.java
@@ -28,6 +28,7 @@ public class SystemManagerApplication {
     public static void main(String[] args) {
         System.out.println("zsp branch master update");
         System.out.println("zsp branch zsp update");
+        System.out.println("master update push");
         SpringApplication.run(SystemManagerApplication.class);
 
     }
